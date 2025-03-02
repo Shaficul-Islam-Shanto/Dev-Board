57908b5a7d563c4f87d0b627b87f4e7c52c9f67c
Shaficul-Islam-Shanto <shantoshaficul141@gmail.com>
2025-03-02 13:10:46 +0600

    Main javascript section added

--- buttonChange.js
+++ buttonChange.js
@@ -1,20 +1,49 @@
 const btnCompleted = document.querySelectorAll(".btn-completed");
+const headings = document.querySelectorAll(".heading");
 
-const totalNumber = document.getElementById("total-number");
+const currentDateElement = document.getElementById("current-date");
+if (currentDateElement) {
+    const currentDate = new Date();
+    currentDateElement.innerText = currentDate.toDateString();
+}
+for (let i = 0; i < btnCompleted.length; i++) {
+    btnCompleted[i].addEventListener("click", function () {
+        alert("Board Updated Successfully.");
 
-for (let btn of btnCompleted) {
-    btn.addEventListener("click", function () {
         let taskElement = document.getElementById("task-number");
         let taskNumber = parseInt(taskElement.innerText);
         let totalValue = document.getElementById("total-number");
         let totalNumber = parseInt(totalValue.innerText);
+
         if (taskNumber > 0) {
             taskNumber -= 1;
             totalNumber += 1;
             taskElement.innerText = taskNumber;
             totalValue.innerText = totalNumber;
         }
-        btn.classList.add("disabled");
-    })
 
-}
+        btnCompleted[i].classList.add("disabled");
+
+        const heading = headings[i];
+        if (heading) {
+            const activityInfo = document.getElementById("activity-info");
+            const currentDate = new Date();
+            const formattedTime = currentDate.toLocaleTimeString();
+            const p = document.createElement("p");
+            p.innerText = `
+            You have completed the task "${heading.innerText}" at ${formattedTime}.
+            `;
+            activityInfo.appendChild(p);
+        }
+    });
+} const clearHistoryBtn = document.querySelector(".clear-history-btn");
+
+clearHistoryBtn.addEventListener("click", function () {
+    const activityInfo = document.getElementById("activity-info");
+    activityInfo.innerHTML = '';
+});
+
+
+
+
+
